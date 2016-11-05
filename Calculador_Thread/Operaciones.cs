5f147dedccc77afbf196d43b3b3fb7d020680a73
Nelson Venegas <nvenegar@hotmail.com>
2016-11-04 22:58:05 -0500

    Agregar archivos de proyecto.

---
+++ Calculador_Thread/Operaciones.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculador_Thread
+{
+   class Operaciones
+    {
+        public int operador1;
+        public int operador2;
+        private int resultado;
+
+        // Constructor de la clase, la cual toma valores iniciales
+
+        public Operaciones(int _operador1, int _operador2)
+        {
+            operador1=_operador1;
+            operador2=_operador2;
+        }
+
+        public void Sumar()
+        {
+            lock (this)
+            {
+                resultado = operador1 + operador2;
+                //Bloqueamos el Hilo actual durante un segundo
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Suma es:" + resultado);
+            }
+       }
+
+        public void Restar()
+        {
+            lock (this)
+            {
+                resultado = operador1 - operador2;
+                //Bloqueamos el Hilo actual durante un segundo
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Resta es:" + resultado);
+            }
+        }
+
+        public void Multiplicar()
+        {
+            lock (this)
+            {
+                resultado = operador1 * operador2;
+                //Bloqueamos el hilo actual durante un segundo
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Multiplicacion es:" + resultado);
+            }
+        }
+    }
+}
---
+++ Calculador_Thread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculador_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion de la instancia de la clase e inicializacion de la misma.
+            Operaciones Op = new Operaciones(5, 7);
+            //Creacion de cada uno de los hilos para cada una de las operaciones de la clase
+            Thread Hilo1 = new Thread(new ThreadStart(Op.Sumar));
+            Thread Hilo2 = new Thread(new ThreadStart(Op.Restar));
+            Thread Hilo3 = new Thread(new ThreadStart(Op.Multiplicar));
+            //Inicio de ejecucion de cada uno de los hilos creados
+            Hilo1.Start();
+            Hilo2.Start();
+            Hilo3.Start();
+            //Espera para que el usuario presione una tecla. 
+            Console.ReadKey();
+
+        }
+    }
+}
